8ba54a6fc7cbbf53113ce8621f8f6c50e1c363eb
= <alexandrecaus@hotmail.com>
2022-07-31 14:00:46 -0300

    make it better 4

--- IQ-AlertSignal.py
+++ IQ-AlertSignal.py
@@ -42,15 +42,15 @@ def Assets_Refresh_Openeds():
     global assets_opened
     assets_opened=API.get_all_open_time()
 
-def Assets_Care_CheckAsset(asset_name):
+def AssetsCare_CheckAsset(asset_name, asset_class):
     for i in range(0, len(assets_care)):
-        if assets_care[i]["name"] == asset_name:
+        if assets_care[i]["name"] == asset_name and assets_care[i]["class"] == asset_class:
             return True
     
     return False
 
 
-def Assets_Care_Update():
+def AssetsCare_Update():
 
     url = "https://www.smart2trader.com/api/tiago_iq_asset_alert/GetAssets.php"
 
@@ -61,35 +61,80 @@ def Assets_Care_Update():
 
     json = r.json()
 
+    print("> Dados obtidos: ")
+    print(json)
+
     if json["status"] != "success":
+        return False
+
+    try:
+        len(json["msg"])
+    except:
+        print("> Não existem ativos para acompanhar..")
         return False
 
     for i in range(0, len(json["msg"])):
         asset_name = json["msg"][i]["name"]
         asset_class = json["msg"][i]["class"]
-        if Assets_Care_CheckAsset(asset_name) == False:
+
+        if AssetsCare_CheckAsset(asset_name, asset_class) == False:
             assets_len = len(assets_care)
             assets_care.append(assets_len)
-            assets_care[0]["name"] = asset_name
-            assets_care[0]["class"] = asset_class
-            assets_care[0]["on"] = False
+            assets_care[assets_len] = {}
+            assets_care[assets_len]["name"] = asset_name
+            assets_care[assets_len]["class"] = asset_class
+            assets_care[assets_len]["on"] = False
 
-    print("> Assets_Care_Update")
-    print(assets_care)
+    for i in range(0, len(assets_care)):
+
+        if i > len(assets_care)-1: 
+            continue
+
+        asset_class = assets_care[i]["class"]
+        asset_name = assets_care[i]["name"]
+
+        found = False
+
+        for i2 in range(0, len(json["msg"])):
+            if assets_care[i]["name"] == json["msg"][i2]["name"] and assets_care[i]["class"] == json["msg"][i2]["class"]:
+                found = True 
+    
+        if found == False:
+            print("Deixou de existir ativo: ", asset_name)
+            assets_care.pop(i)
+
+
+def Telegram_Alertar(mensagem):
+    token = "5143782959:AAFnoNU58LuHvpNMOKtFlf4V2QXxxi75SfU"
+    url = f"https://api.telegram.org/bot{token}"
+    params = {"chat_id": "-1001661767638", "text": mensagem}
+    r = requests.get(url + "/sendMessage", params=params)
+    print("> Notificação Telegram Enviada")
+    print("- " + mensagem)
         
-
 assets_care = []
 
 while True:
-    
-    Assets_Refresh_Openeds()
-    Assets_Care_Update()
 
-    for i in range(0, len(assets_care)):
-        print("Verificando ativo | "+assets_care[i]["name"]+" / " + assets_care[i]["class"])
-        print(assets_opened[assets_care[i]["class"]][assets_care[i]["name"]]["open"])
-        time.sleep(10)
-        pass
-
+    time.sleep(5)
     #time.sleep(60*15) # 15 minutos
     
+    Assets_Refresh_Openeds()
+
+    if AssetsCare_Update() == False:
+        continue
+
+    print("")
+
+    for i in range(0, len(assets_care)):
+        status = assets_opened[assets_care[i]["class"]][assets_care[i]["name"]]["open"]
+        print("> Verificando ativo | "+assets_care[i]["name"]+" / " + assets_care[i]["class"])
+
+        if assets_care[i]["on"] == False and status == True:
+            # Envia notificação
+            print("> Enviando notificação..")
+            Telegram_Alertar(assets_care[i]["name"]+"-"+assets_care[i]["class"]+" | Disponível!")
+            assets_care[i]["on"] = True    
+
+
+        assets_care[i]["on"] = status
--- teste.py
+++ teste.py
@@ -1,12 +1,2 @@
-import requests
-
-url = "https://www.smart2trader.com/api/tiago_iq_asset_alert/GetAssets.php"
-
-r = requests.get(url)
-
-json = r.json()
-
-for i in range(0, len(json["msg"])):
-    print(json["msg"][i]["name"])
-
-#print(json["msg"][0]["name"])
+if "teste" == "teste":
+    print("IGUAL!")
